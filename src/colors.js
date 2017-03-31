0956741b8a56f76d232eb399b5cc99d2efa2a297
flubox <francois.lunette@photobox.com>
2017-03-31 17:19:03 +0200

    colors parsing

--- src/colors.js
+++ src/colors.js
@@ -1,7 +1,5 @@
-import { getGroupsWithId, nodeList2Array } from './parser';
+import { capitalizeFirstLetter, getGroupsWithId, nodeList2Array } from './parser';
 import convertCssColorNameToHex from 'convert-css-color-name-to-hex';
-
-export const capitalizeFirstLetter = string => `${string.toUpperCase().substr(0, 1)}${string.toLowerCase().substr(1)}`;
 
 export const filterColorById = g => typeof g.id !== 'undefined' && g.id.indexOf('color') === 0;
 
@@ -9,7 +7,7 @@ export const getShortRgbValue = rawRgbValue => rawRgbValue.length < 6 ? rawRgbVa
 
 export const getRgb = color => color.indexOf('#') === 0 ? color.replace(/#/g, '') : getRgb(convertCssColorNameToHex(color));
 
-export const getGroupColorByText = text => text.getAttribute('fill');
+export const getRectFillColor = rect => rect.getAttribute('fill');
 
 export const parseColorType = explicitColorDeclaration => explicitColorDeclaration.indexOf(':') > 0 ? explicitColorDeclaration.split(':').slice(1) : explicitColorDeclaration;
 
@@ -19,19 +17,22 @@ export const getRgbFromColorPicker = group => {
     return colorPicker === null ? '000000' : colorPicker.getAttribute('fill');
 };
 
-export const getColorTypeDeclaration = text => text.textContent.split(':');
+export const getColorTypeDeclaration = ({ id }) => {
+    if (id.indexOf(':') > -1) {
+        return getColorTypeDeclaration({ id: id.split(':')[1] });
+    }
+    return id.split('_').map(a => a.trim());
+};
 
 export const filterColorPrefix = color => color !== 'color';
 
-export const getColorsFromTexts = texts => {
-    return nodeList2Array(texts).map(text => {
-            const rawColorTypeDeclaration = getColorTypeDeclaration(text);
-            const split = a => a.split(' ');
+export const getColorsFromRects = rects => {
+    return nodeList2Array(rects).map(rect => {
             const reduce = (a, b) => a.concat(b);
-            const result = {
-                colorType: rawColorTypeDeclaration.filter(filterColorPrefix).map(split).reduce(reduce, []),
-                rgb: getRgb(getGroupColorByText(text))
-            };
+            const colorType = getColorTypeDeclaration(rect).filter(a => a.length);
+            const rgb = getRgb(getRectFillColor(rect));
+            const result = { colorType, rgb };
+            console.info('...', rect, result, colorType, rgb);
             return result;
         })
         .reduce((a, b) => a.concat(b.colorType.length ? b.colorType.map(colorType => ({...b, colorType })) : b), []);
