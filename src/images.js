9335cbfe52e4d28cd1458107f5c8bebe8cae6a15
flubox <Francois.lunette@photobox.com>
2017-04-05 15:42:33 +0200

    Update images.js

--- src/images.js
+++ src/images.js
@@ -16,21 +16,21 @@ export const makeSvg = svgClipart => {
 
 export const svgAsString = svg => svg.outerHTML;
 
-export const uploadSvg = filename => Body => S3 => S3.upload(getSvgUploadOptions(filename)(Body));
+export const uploadSvg = filename => Body => upload => upload(getSvgUploadOptions(filename)(Body));
 
 export const getSvgUrl = previous => data => ({...previous, urlSvg: getLocation(data) });
 
 export const getS3Filepath = filename => image => image && image.id ? `${filename}/${image.id.match(/([a-z0-9])*/gi).join('_')}.svg` : filename;
 
-export const getSvgThumbnails = filename => image => S3 => uploadSvg(getS3Filepath(filename)(image))(svgAsString(makeSvg(image)))(S3).promise();
+export const getSvgThumbnails = filename => image => upload => uploadSvg(getS3Filepath(filename)(image))(svgAsString(makeSvg(image)))(upload).promise();
 
-export const parseImagesFromSVG = filename => svg => S3 => hashFunction => {
+export const parseImagesFromSVG = filename => svg => upload => hashFunction => {
     const useHashFunction = typeof hashFunction === 'function';
     const imagesGroup = nodeList2Array(svg.querySelectorAll('g[id="images"] [id*="image"]'));
     const hash = data => ({...data, hash: hashFunction(JSON.stringify(data)) });
     const addHashIfNeeded = data => resolve => resolve(useHashFunction ? hash(data) : data);
     const resolveWithHash = data => Promise.resolve({ then: addHashIfNeeded(data) });
-    return Promise.all(imagesGroup.map(image => getSvgThumbnails(filename)(image)(S3)
+    return Promise.all(imagesGroup.map(image => getSvgThumbnails(filename)(image)(upload)
         .then(getSvgUrl({ imageType: getImageType(image) }))
         .then(resolveWithHash)
     ));
