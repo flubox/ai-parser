63a108f1a746b6e67343c4070aabe0a16688f0a0
flubox <francois.lunette@photobox.com>
2017-04-11 14:33:11 +0200

    parsing info: designs & toolkit, indexed loaders

--- src/client.js
+++ src/client.js
@@ -7,9 +7,10 @@ window.svgParserClient = domElement => endpoints => {
     let data = [];
     const onDrop = files => {
         const reader = new FileReader();
-        files.map((file, index) => {
+        files.map(file => {
             const loader = document.createElement('div');
             const info = document.createElement('div');
+            const index = document.querySelectorAll('[id*="loader-"]').length;
             const loaderId = `loader-${index}`;
             const infoId = `info-${index}`;
             loader.setAttribute('id', loaderId);
@@ -23,6 +24,7 @@ window.svgParserClient = domElement => endpoints => {
                     let png;
                     const svg = target.result;
                     loader.innerHTML = svg;
+                    // loader
                     document.querySelector(domElement).appendChild(loader);
                     document.querySelector(`#${loaderId}`).appendChild(info);
                     const S3 = new AWS.S3();
@@ -50,62 +52,80 @@ window.svgParserClient = domElement => endpoints => {
                         });
                     };
 
-                    parser(document.querySelector(`#${loaderId} svg`))(options).then(parsed => {
-                        console.info('####', 'parsed', {...parsed });
-                        const tmpPreview = document.createElement('embed');
-                        tmpPreview.setAttribute('src', parsed.urlThumb);
-                        document.querySelector(`#${loaderId} svg`).outerHTML = tmpPreview.outerHTML;
+                    const onParsed = {
+                        toolkit: toolkit => {
+                            console.info('####', 'toolkit', {...toolkit });
+                            const tmpPreview = document.createElement('embed');
+                            tmpPreview.setAttribute('src', toolkit.urlThumb);
+                            tmpPreview.style.width = '50%';
+                            document.querySelector(`#${loaderId} svg`).outerHTML = tmpPreview.outerHTML;
 
-                        const containerEl = document.createElement('div');
-                        containerEl.setAttribute('class', 'mui-container-fluid');
-                        const panelEl = document.createElement('div');
-                        panelEl.setAttribute('class', 'mui-panel');
-                        const titleEl = document.createElement('h2');
-                        titleEl.textContent = parsed.title;
-                        panelEl.appendChild(titleEl);
+                            const containerEl = document.createElement('div');
+                            containerEl.setAttribute('class', 'mui-container-fluid');
+                            const panelEl = document.createElement('div');
+                            panelEl.setAttribute('class', 'mui-panel');
+                            const titleEl = document.createElement('h2');
+                            titleEl.textContent = toolkit.title;
+                            panelEl.appendChild(titleEl);
 
-                        titleForArray('colors')(panelEl);
-                        Array2Dom('colors')(parsed.colors)(panelEl)((el, i, each, tmpEl) => {
-                            if (each.rgb && !tmpEl.querySelector('input')) {
-                                const tmpInput = document.createElement('input');
-                                tmpInput.setAttribute('type', 'color');
-                                tmpInput.setAttribute('disabled', 'disabled');
-                                tmpInput.value = `#${each.rgb}`;
-                                tmpEl.appendChild(tmpInput);
-                            }
-                            return el;
-                        });
-
-                        titleForArray('fonts')(panelEl);
-                        Array2Dom('fonts')(parsed.fonts)(panelEl)((el, i) => {
-                            el.style.fontFamily = parsed.fonts[i].fontName;
-                            el.style.fontSize = '1.5em';
-                            return el;
-                        });
-
-                        titleForArray('images')(panelEl);
-                        Array2Dom('images')(parsed.images)(panelEl)((el, i, each, tmpEl) => {
-                            if (el.classList.value.indexOf('urlSvg') > -1) {
-                                const tmpAnchor = document.createElement('a');
-                                tmpAnchor.setAttribute('href', el.textContent.split(':').slice(1).join(':'));
-                                tmpAnchor.setAttribute('target', '_blank');
-                                tmpAnchor.innerHTML = el.outerHTML;
-                                return tmpAnchor;
-                            }
-                            if (each.urlSvg) {
-                                if (!tmpEl.querySelector('embed')) {
-                                    const tmpPreview = document.createElement('embed');
-                                    tmpPreview.setAttribute('class', 'preview');
-                                    tmpPreview.setAttribute('src', each.urlSvg);
-                                    tmpEl.appendChild(tmpPreview);
+                            titleForArray('colors')(panelEl);
+                            Array2Dom('colors')(toolkit.colors)(panelEl)((el, i, each, tmpEl) => {
+                                if (each.rgb && !tmpEl.querySelector('input')) {
+                                    const tmpInput = document.createElement('input');
+                                    tmpInput.setAttribute('type', 'color');
+                                    tmpInput.setAttribute('disabled', 'disabled');
+                                    tmpInput.value = `#${each.rgb}`;
+                                    tmpEl.appendChild(tmpInput);
                                 }
-                            }
-                            return el;
-                        });
+                                return el;
+                            });
 
-                        containerEl.appendChild(panelEl);
-                        document.querySelector(`#${infoId}`).appendChild(containerEl);
+                            titleForArray('fonts')(panelEl);
+                            Array2Dom('fonts')(toolkit.fonts)(panelEl)((el, i) => {
+                                el.style.fontFamily = toolkit.fonts[i].fontName;
+                                el.style.fontSize = '1.5em';
+                                return el;
+                            });
+
+                            titleForArray('images')(panelEl);
+                            Array2Dom('images')(toolkit.images)(panelEl)((el, i, each, tmpEl) => {
+                                const urlSvg = el.classList.value.indexOf('urlSvg') > -1;
+                                const urlThumb = el.classList.value.indexOf('urlThumb') > -1;
+                                const urlScale = el.classList.value.indexOf('urlScale') > -1;
+                                const urlFull = el.classList.value.indexOf('urlFull') > -1;
+                                const url = urlSvg || urlThumb || urlScale || urlFull;
+
+                                if (url) {
+                                    const tmpAnchor = document.createElement('a');
+                                    tmpAnchor.setAttribute('href', el.textContent.split(':').slice(1).join(':'));
+                                    tmpAnchor.setAttribute('target', '_blank');
+                                    tmpAnchor.innerHTML = el.outerHTML;
+                                    return tmpAnchor;
+                                }
+                                if (each.urlSvg) {
+                                    if (!tmpEl.querySelector('embed')) {
+                                        const tmpPreview = document.createElement('embed');
+                                        tmpPreview.setAttribute('class', 'preview');
+                                        tmpPreview.setAttribute('src', each.urlSvg);
+                                        tmpEl.appendChild(tmpPreview);
+                                    }
+                                }
+                                return el;
+                            });
+
+                            containerEl.appendChild(panelEl);
+                            document.querySelector(`#${infoId}`).appendChild(containerEl);
+                        },
+                        designs: designs => {
+
+                        }
+                    }
+
+                    parser(document.querySelector(`#${loaderId} svg`))(options).then(parsed => {
+                        console.info('###', 'parsed', parsed);
+                        Object.keys(parsed).map(key => onParsed[key](parsed[key]));
                     });
+                    // parse.toolkit(document.querySelector(`#${loaderId} svg`))(options).then(onParsed.toolkit);
 
 
                 };
