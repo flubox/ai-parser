1c5b2c81b48d4f555aeedff3288be3a005e4ce03
flubox <francois.lunette@photobox.com>
2017-04-11 14:34:38 +0200

    placeholder for designs parsing, full parsing obj

--- src/parser.js
+++ src/parser.js
@@ -27,23 +27,58 @@ export const checkMode = groups => {
     return 'flu';
 };
 
+export const checkContent = svg => {
+    console.info('...', 'checkContent', typeof svg);
+    const toolkit = !!svg.querySelectorAll('g#toolkit').length;
+    const designs = !!svg.querySelectorAll('g#designs').length;
+    console.info('...', 'checkContent', {toolkit, designs});
+    return {toolkit, designs};
+};
+
 export const nodeList2Array = nodeList => [].slice.call(nodeList);
 
+export const parse = {
+    toolkit: svg => options => {
+        const { filename, S3, hashFunction, hashMethod } = options;
+        const groups = nodeList2Array(svg.querySelectorAll('g#toolkit g'));
+        const mode = checkMode(groups);
+        const colorsGroup = nodeList2Array(svg.querySelectorAll('rect[id*="color"]'));
+        const fontsGroup = nodeList2Array(svg.querySelectorAll('g[id="fonts"] text'));
+        const imagesGroup = nodeList2Array(svg.querySelectorAll('[id="images"] [id="image"]'));
+        const urlThumbPath = `${filename}/${filename.replace('.svg', '.thumb.svg')}`;
+        return new Promise((resolve, reject) => {
+            Promise.all([
+                new Promise((resolve, reject) => S3.upload(getSvgUploadOptions(urlThumbPath)(svg.outerHTML)).promise().then(getLocation).then(urlThumb => resolve({ urlThumb }))),
+                Promise.resolve({ title: svg.querySelector('title').textContent }),
+                Promise.resolve({ colors: getColorsFromRects(colorsGroup)({hashFunction, hashMethod}) }),
+                Promise.resolve({ fonts: getFontsFromGroups(fontsGroup)({hashFunction, hashMethod}) }),
+                new Promise((resolve, reject) => parseImagesFromSVG(filename)(svg)(S3)({hashFunction, hashMethod}).then(images => resolve({ images }))),
+            ]).then(values => resolve({toolkit: values.reduce(merge, {})}))
+        });
+    },
+    designs: svg => options => {
+      return new Promise((resolve, reject) => {
+          resolve({designs: []});
+      });
+    }
+};
+
 export const parser = svg => options => {
+    if (typeof svg === 'string') {
+        const svgElement = document.createElement('div');
+        svgElement.innerHtml = svg;
+        return parser(svgElement)(options);
+    }
+
     const { filename, S3, hashFunction, hashMethod } = options;
-    const groups = nodeList2Array(svg.querySelectorAll('g'));
-    const mode = checkMode(groups);
-    const colorsGroup = nodeList2Array(svg.querySelectorAll('rect[id*="color"]'));
-    const fontsGroup = nodeList2Array(svg.querySelectorAll('g[id="fonts"] text'));
-    const imagesGroup = nodeList2Array(svg.querySelectorAll('[id="images"] [id="image"]'));
-    const urlThumbPath = `${filename}/${filename.replace('.svg', '.thumb.svg')}`;
+
     return Promise.all([
-        new Promise((resolve, reject) => S3.upload(getSvgUploadOptions(urlThumbPath)(svg.outerHTML)).promise().then(getLocation).then(urlThumb => resolve({ urlThumb }))),
-        Promise.resolve({ title: svg.querySelector('title').textContent }),
-        Promise.resolve({ colors: getColorsFromRects(colorsGroup)({hashFunction, hashMethod}) }),
-        Promise.resolve({ fonts: getFontsFromGroups(fontsGroup)({hashFunction, hashMethod}) }),
-        new Promise((resolve, reject) => parseImagesFromSVG(filename)(svg)(S3)({hashFunction, hashMethod}).then(images => resolve({ images }))),
-    ]).then(values => values.reduce(merge, {}));
+        parse.toolkit(svg)(options),
+        parse.designs(svg)(options)
+    ])
+    .then(values => values.reduce(merge, {}))
+    ;
+
 };
 
 export default parser;
