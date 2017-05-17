2464d0703f2935dda3c36f6251498f5eb442eda4
flubox <francois.lunette@photobox.com>
2017-05-17 11:31:58 +0200

    webpack config update for multiple entries, browser sync

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,11 @@
 var path = require('path');
 var webpack = require('webpack');
+var BrowserSyncPlugin = require('browser-sync-webpack-plugin');
 
 module.exports = {
-    entry: ['./src/parser.js'],
     entry: {
         index: path.resolve(__dirname, 'src/parser.js'),
-        demo: path.resolve(__dirname, 'src/client.js')
+        client: path.resolve(__dirname, 'src/client.js')
     },
     output: {
         path: path.resolve('dist'),
@@ -27,5 +27,14 @@ module.exports = {
             }
         ]
     },
-    devtool: 'source-map'
+    devtool: 'source-map',
+    plugins: [
+        new BrowserSyncPlugin({
+            host: 'localhost',
+            port: 3000,
+            server: {
+            baseDir: ['./']
+            }
+        })
+    ]
 };
