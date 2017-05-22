e9576b3495c6b24663035cc411bd44402df38e0e
flubox <francois.lunette@photobox.com>
2017-05-22 12:04:07 +0200

    cleaner

--- src/helper.js
+++ src/helper.js
@@ -1,11 +1,9 @@
 export const all = key => value => list => list.every(item => isDef(item[key]) && item[key] === value);
-
 export const allType = value => list => all('type')(value)(list);
-
+export const camel = text => `${uCase(0)(1)(text)}${lCase(1)(text)}`;
 export const capitalizeFirstLetter = string => `${string.toUpperCase().substr(0, 1)}${string.toLowerCase().substr(1)}`;
-
+export const clean = data => Object.keys(data).reduce((all, k) => shouldAdd(data[k]) ? ({...all, [k]: data[k]}) : all, {});
 export const concat = (a, b) => a.concat(b);
-
 export const extract = targetName => json => {
     const {name, elements} = json;
     if (unDef(name) && name === targetName) {
@@ -15,30 +13,24 @@ export const extract = targetName => json => {
     }
     return [];
 };
-
 export const not = a => b => Array.isArray(a) ? !a.includes(b) : a !== b;
-
 export const is = a => b => Array.isArray(a) ? a.includes(b) : a === b;
-
-export const convertToUnit = dimension => viewbox => viewport => value => {
-    // console.info('convertToUnit', dimension, viewbox, viewport, value, viewport[dimension], viewbox[dimension], `${value} * (${viewport[dimension]} / ${viewbox[dimension]})`);
-    if (viewbox[dimension] === 0) {
-        return value;
-    } 
-    return viewport[dimension] === 0 ? value : value * (viewport[dimension] / viewbox[dimension]);
-};
-
-export const toUnit = options => data => {
-    // if (data.type === 'rect') console.info('\n', data.type, data);
-    const {viewbox, viewport} = options;
-    const converted = keys(data).filter(only(['x', 'y', 'width', 'height'])).reduce((accumulator, k) => {
-        const dimension = ['x', 'width'].includes(k) ? 'width' : 'height';
-        // if (data.type === 'rect') console.info('toUnit', k, `dimension:${dimension}`, data[k] === 0, data[k], convertToUnit(dimension)(viewbox)(viewport)(data[k]));
-        return {...accumulator, [k]: data[k] === 0 ? data[k] : convertToUnit(dimension)(viewbox)(viewport)(data[k])};
-    }, {});
-    return {...data, ...converted};
-};
-
+// export const convertToUnit = dimension => viewbox => viewport => value => {
+//     if (viewbox[dimension] === 0) {
+//         return value;
+//     } 
+//     return viewport[dimension] === 0 ? value : value * (viewport[dimension] / viewbox[dimension]);
+// };
+// export const toUnit = options => data => {
+//     // if (data.type === 'rect') console.info('\n', data.type, data);
+//     const {viewbox, viewport} = options;
+//     const converted = keys(data).filter(only(['x', 'y', 'width', 'height'])).reduce((accumulator, k) => {
+//         const dimension = ['x', 'width'].includes(k) ? 'width' : 'height';
+//         // if (data.type === 'rect') console.info('toUnit', k, `dimension:${dimension}`, data[k] === 0, data[k], convertToUnit(dimension)(viewbox)(viewport)(data[k]));
+//         return {...accumulator, [k]: data[k] === 0 ? data[k] : convertToUnit(dimension)(viewbox)(viewport)(data[k])};
+//     }, {});
+//     return {...data, ...converted};
+// };
 export const extractClipPaths = json => {
     const {name, elements} = json;
     const noName = typeof name === 'undefined';
@@ -50,10 +42,7 @@ export const extractClipPaths = json => {
     }
     return [];
 };
-
 export const extactIdFromUrl = url => url.match(/url\(#(.+)\)/i);
-
-
 export const extractSymbols = json => {
     const {name, elements} = json;
     const noName = typeof name === 'undefined';
@@ -65,108 +54,38 @@ export const extractSymbols = json => {
     }
     return [];
 };
-
-// export const extractDefs = json => {
-//     const symbols = reduceByMerge(extractSymbols(json).map(s => indexUp(s.attributes.id)(s)));
-//     const clipPath = reduceByMerge(extractClipPaths(json).map(s => indexUp(s.attributes.id)(s)));
-//     const result = {clipPath, symbols};
-//     //     // symbols: reduceByMerge(extractSymbols(json).map(s => indexUp(s.attributes.id)(s))), 
-//     //     symbols: extractSymbols(json), 
-//     //     clipPath: reduceByMerge(extractClipPaths(json).map(s => indexUp(s.attributes.id)(s)))
-//     // };
-//     console.info('...', 'extractDefs', json, 'clipPath', clipPath, 'symbols', symbols);
-//     return result;
-// }
-
 export const extractUnit = size => {
     const match = size.match(/(?:[\.\d]+)([\D]+)/);
     if (match) {
         return match[1];
     }
 };
-
-export const getDefaultRotation = data => new Promise(resolve => resolve({...keysExcept(data)('type').reduce(mergeWith(data), {}), rotation: 0}));
-
-export const getText = ({elements}) => isDef(elements) && elements.length === 1 ? filterUndefinedValues({...elements[0], type: undefined}) : undefined;
-
-export const hasId = json => isDef(json.attributes) && isDef(json.attributes.id);
-
-export const hasAttributes = json => isDef(json.attributes);
-
-// export const is = ({tag, json}) => json.name === tag;
-
+export const encapsulate = (f = a => a) => data => key => ({[key]: f(data[key])});
 export const extractUuids = surface => Array.isArray(surface) ? surface.map(extractUuids) : surface.uuid;
-
 export const extractIds = surface => Array.isArray(surface) ? surface.map(extractIds) : surface.id;
-
 export const extractIdentifiers = debug => surface => debug && hasId(surface) ? extractIds(surface) : extractUuids(surface);
-
 export const filterTag = tag => element => element.name === tag;
-
-export const isArray = data => Array.isArray(data);
-
-export const keys = data => Object.keys(data || {});
-
-export const keysExcept = data => except => keys(data).filter(not(except));
-
-export const reduceByKeys = data => (accumulator, k) => ({...accumulator, [k]: data[k]});
-
 export const filterUndefinedValues = data => isArray(data) ? data.map(filterUndefinedValues) : keys(data || {}).filter(k => isDef(data[k])).reduce(reduceByKeys(data), {});
-
 export const filterEmptySurfaces = data => unDef(data.surfaces) ? data : (Object.keys(data.surfaces).length ? data : filterUndefinedValues({...data, surfaces: undefined}));
-
-export const getSubGroupsWithId = svg => svg.querySelectorAll('g[id]');
-
-export const getTypeFromId = id => id.split(/[-_]/)[0];
-
-export const matchName = name => element => element.name === name;
-
-export const isNode = obj => obj instanceof Node;
-
 export const filterElementsByName = name => obj => obj.elements.filter(matchName);
-
+export const float = value => parseFloat(value);
+export const first = array => array[0];
+export const getDefaultRotation = data => new Promise(resolve => resolve({...keysExcept(data)('type').reduce(mergeWith(data), {}), rotation: 0}));
+export const getText = ({elements}) => isDef(elements) && elements.length === 1 ? filterUndefinedValues({...elements[0], type: undefined}) : undefined;
+export const hasId = json => isDef(json.attributes) && isDef(json.attributes.id);
+export const hasAttributes = json => isDef(json.attributes);
+export const isArray = data => Array.isArray(data);
+export const keys = data => Object.keys(data || {});
+export const keysExcept = data => except => keys(data).filter(not(except));
+export const lCase = (start = 0) => text => text.substr(start).toLowerCase();
+export const reduceByKeys = data => (accumulator, k) => ({...accumulator, [k]: data[k]});
 export const get = name => obj => isNode(obj) ? obj.querySelectorAll(name) : (obj.elements ? filterElementsByName(name)(obj) : undefined);
-
 export const getSubGroups = obj => get('g')(obj);
-
-// export const getColor = json => {
-//     return new Promise((resolve, reject) => {
-//         if (hasAttributes(json)) {
-//             const {fill} = json.attributes;
-//             return resolve({fill});
-//         }
-//         reject();
-//     });
-// };
-
-// export const getPosition = json => {
-//     return new Promise((resolve, reject) => {
-//         if (hasAttributes(json)) {
-//             const {x, y} = json.attributes;
-//             return resolve({x, y});
-//         }
-//         reject();
-//     });
-// };
-
-// export const getSize = json => {
-//     return new Promise((resolve, reject) => {
-//         if (hasAttributes(json)) {
-//             const {height, width} = json.attributes;
-//             return resolve({height, width});
-//         }
-//         reject();
-//     });
-// };
-
+export const getSubGroupsWithId = svg => svg.querySelectorAll('g[id]');
+export const getTypeFromId = id => id.split(/[-_]/)[0];
 export const getRects = obj => get('rect')(obj);
-
 export const getTexts = obj => get('text')(obj);
-
 export const getUse = ({clipPath}) => ({attributes, elements}) => unDef(attributes) || unDef(elements) ? undefined : first(first(elements).elements);
-
-export const indexUp = key => object => ({[object[key] || [key]]: object});
-
 export const getViewBox = json => {
     if (json && json.attributes && json.attributes.viewBox) {
         const {viewBox} = json.attributes;
@@ -176,49 +95,26 @@ export const getViewBox = json => {
     }
     return false;
 };
-
-export const float = value => parseFloat(value);
-
+export const indexUp = key => object => ({[object[key] || [key]]: object});
+export const isNode = obj => obj instanceof Node;
+export const matchName = name => element => element.name === name;
+export const named = value => data => data.name === value;
 export const only = keys => key => keys.includes(key);
-
-export const encapsulate = (f = a => a) => data => key => ({[key]: f(data[key])});
-
-export const toFloat = data => ({...data, ...keys(data).filter(only(['x', 'y', 'width', 'height'])).map(encapsulate(float)(data)).reduce(merge, {})});
-
 export const isDef = item => !unDef(item);
-
 export const unDef = item => typeof item === 'undefined';
-
-export const first = array => array[0];
-
 export const merge = (a, b) => ({...a, ...b });
-
 export const mergeWith = data => (accumulator, key) => ({...accumulator, [key]: data[key]});
-
 export const nodeList2Array = nodeList => [].slice.call(nodeList);
-
 export const reduceByConcat = list => Array.isArray(list) ? list.reduce(concat, []) : list;
-
 export const reduceByMerge = list => Array.isArray(list) ? list.reduce(merge, {}) : list;
-
 export const resolver = options => (data, f) => f(options)(data);
-
 export const rise = options => resolvers => data => resolvers.reduce(resolver(options), data);
-
 export const riseSurfaces = ({debug}) => data => isDef(data.surfaces) ? [{...data, surfaces: data.surfaces.map(extractIdentifiers(debug))}].concat(data.surfaces.map(riseSurfaces)) : data;
-
-export const useClipPath = defs => id => Array.isArray(id) ? reduceByMerge(id.map(useClipPath(defs))) : ({...defs.clipPath[id]});
-
-export const uCase = (start = 0) => (end = 1) => text => text.substr(start, end).toUpperCase;
-
-export const lCase = (start = 0) => text => text.substr(start).toLowerCase();
-
-export const camel = text => `${uCase(0)(1)(text)}${lCase(1)(text)}`;
-
 export const remove = key => data => filterUndefinedValues({...data, [key]: undefined});
-
 export const removeAttributes = data => remove('attributes')(data);
-
 export const removeElements = data => remove('elements')(data);
-
+export const shouldAdd = data => isDef(data);
 export const sortByZindex = (a, b) => a.z < b.z ? -1 : 1;
+export const useClipPath = defs => id => Array.isArray(id) ? reduceByMerge(id.map(useClipPath(defs))) : ({...defs.clipPath[id]});
+export const uCase = (start = 0) => (end = 1) => text => text.substr(start, end).toUpperCase;
+export const toFloat = data => ({...data, ...keys(data).filter(only(['x', 'y', 'width', 'height'])).map(encapsulate(float)(data)).reduce(merge, {})});
--- src/parserLayoutSet.js
+++ src/parserLayoutSet.js
@@ -1,70 +1,70 @@
 const uuidV4 = require('uuid/v4');
 import {chain, combine, combineArray, from, fromPromise, just} from 'most';
 import * as helper from './helper';
-import {concat, first, isDef, keys, merge} from './helper';
+import {clean, concat, first, isDef, keys, merge, named, shouldAdd, toFloat} from './helper';
 import {extractPhysicalSize, mergeResults} from './surfaces';
 import {getProductDeclaration} from './product';
 
 export const encapsulate = key => data => ({[key]: data});
 
-export const extractAttributes = element => ({...element, ...element.attributes, attributes: undefined});
+export const extractAttributes = element => ({...element.attributes});
 
 export const parseElements = options => parser => json => encapsulate('elements')(isDef(json.elements) ? ({...json, elements: json.elements.map(parser)}) : undefined);
 
-export const removeDataId = data => ({...data, 'data-id': undefined});
+export const extractTransform = data => {
+    if (isDef(data.transform)) {
+        const match = data.transform.match(/(translate)\(([\d\.]+)\s([\d\.]+)\)/)
+        if (match) {
+            const transform = {[match[1]]: toFloat({x: match[2], y: match[3]})};
+            console.info('...', 'match', match, transform);
+            data = ({...data, transform});
+        }
+    }
+    return data;
+};
 
 export const addUuid = data => ({...data, uuid: uuidV4()});
 
+export const hasGroups = data => isDef(data.elements) && data.elements.length > 0;
+
+export const hasSubGroup = data => hasGroups(data) && data.elements.some(named('g'));
+
+export const isTitle = data => named('title')(data) && isDef(data.elements) && data.elements.length === 1;
+
+export const shouldSkipGroup = data =>  hasSubGroup(data);
+
+export const popGroup = data => ({...data, elements: data.elements.map(e => named('g')(e) ? e.elements : e).reduce(concat, [])})
+
 export const parseLayout = options => json => {
-    // console.info('parseLayout', json);
-
-    const elements = isDef(json.elements) ? json.elements.map(parseLayout(options)) : [];
-    const merge = a => ({...a, elements});
-    const result = [
-        extractAttributes,
-        merge,
-        removeDataId,
-        helper.filterUndefinedValues,
-        addUuid
-    ].reduce((a, f) => ({...a, ...f(json)}), json);
-
-    console.info('...', 'result', result);
-
-    return result;
-
-    // const data = just(json)
-    // .map(extractAttributes)
-    // .map(parseElements(parseLayout(options)))
-    // .map(helper.filterUndefinedValues)
-    // .map(addUuid)
-
-    // return data;
     
-    // layout
-    // .forEach(console.info.bind(console))
-    
-// export const parseLayout = ({options, json}) => {
-    // return new Promise(resolve => {
-    //     const attributes = isDef(json) && isDef(json.attributes) ? json.attributes : {};
-    //     const elements = isDef(json) && isDef(json.elements) ? json.elements : [];
-    //     console.info('parseLayout', options, json, attributes, elements);
-    //     Promise.all([
-    //         Promise.resolve({
-    //             ...attributes,
-    //             name: attributes['data-name'],
-    //             uuid: uuidV4(),
-    //             raw: {...json},
-    //             elements,
-    //             attributes: undefined
-    //         })
-    //     ])
-    //     .then(helper.filterUndefinedValues)
-    //     .then(first)
-    //     .then(parseElements)
-    //     .then(layout => {
-    //         resolve({[layout.uuid]: layout});
-    //     })
-    // });
+    if (isTitle(json)) {
+        if (options.debug) console.info('°°°°°°°°°°°°°°°°°°°°°°°°°°°', 'json is title');
+        return clean({...json, ...json.elements[0], elements: undefined});
+    }
+
+    if (shouldSkipGroup(json)) {
+        if (options.debug) console.info('°°°°°°°°°°°°°°°°°°°°°°°°°°°', 'group should be skipped');
+        return parseLayout(options)(popGroup(json));
+    }
+
+    if (isDef(json.attributes)) {
+        return parseLayout(options)(clean({...json, ...json.attributes, attributes: undefined}));
+    }
+
+    if (isDef(json.elements)) {
+        json = clean({...json, elements: json.elements.map(parseLayout(options))});
+    }
+
+    if (isDef(json.transform)) {
+        json = extractTransform(json);
+    }
+
+    json = toFloat(json);
+
+    console.info('\n parseLayout', json);
+    console.info('@@@', json);
+
+    return json;
 };
 
 export const removeUnit = unit => data => data.replace(unit, '');
@@ -97,45 +97,7 @@ export const parseLayoutSet = ({svg, json}) => options => {
             }
         };
         console.info('layouts', layouts);
-
-        // .then(layoutStreams => {
-        //     const final = combineArray(a => a, layoutStreams)
-        //     console.info('final', final);
-        // })
-
-        // return Promise.all(parsedLayoutSet)
-        // .then(result => {
-        //     console.info('...', result);
-        // })
-
-        // const final = combine(merge, base, parsedLayoutSet)
-
-        // return Promise.all(
-        //     sub.map(element => parseLayout({options, json: element}))
-        //     // [parseLayout({options, json: sub})]
-        // )
-        // .then(result => {
-        //     console.info('parseLayoutSet', 'result', result);
-        //     const {width, height} = helper.toFloat(options);
-        //     const final = {
-        //         [productDetected]: {
-        //             layouts: result.reduce(merge, {}),
-        //             unit,
-        //             width, height
-        //         }
-        //     };
-        //     console.info('parseLayoutSet', 'final', final);
-        //     // const final = {
-        //     //     [productDetected]: {
-        //     //         ...result
-        //     //         uuid: uuidV4(),
-        //     //         product: 'layouts',
-        //     //         type: 'layouts',
-        //     //     }
-        //     // };
-        //     // console.info('...', 'final', final);
-        //     return resolve({then: resolve => resolve(final)});
-        // })
+        resolve(layouts);
     });
 };
 
